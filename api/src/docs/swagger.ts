6456230b5a29837943b50d3ef5c129846ba82f90
beeazy <mutwirievans95@gmail.com>
2025-05-30 17:45:49 +0300

    deploy

--- api/src/docs/swagger.ts
+++ api/src/docs/swagger.ts
@@ -14,7 +14,7 @@ const options: swaggerJsdoc.Options = {
     },
     servers: [
       {
-        url: 'http://localhost:8000',
+        url: 'http://localhost:3000',
         description: 'Development server'
       }
     ],
